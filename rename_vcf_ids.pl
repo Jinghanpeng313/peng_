f57b105f56604df7431604681a032a1afb6a19e2
Jinghanpeng313 <157950263+Jinghanpeng313@users.noreply.github.com>
2024-02-15 22:44:49 +0800

    Create rename_vcf_ids.pl

---
+++ rename_vcf_ids.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+
+if (@ARGV < 1) {
+    die "用法: perl rename_vcf_ids.pl input.vcf > output.vcf\n";
+}
+
+my $filename = $ARGV[0];
+open(my $fh, '<', $filename) or die "无法打开文件: '$filename' $!";
+
+while (my $row = <$fh>) {
+    chomp $row;
+    
+
+    if ($row =~ /^##/) {
+        print "$row\n";
+        next;
+    }
+
+
+    if ($row =~ /^#/) {
+        print "$row\n";
+        next;
+    }
+
+    my @columns = split("\t", $row);
+
+
+    $columns[2] = $columns[0] . "_" . $columns[1];
+
+    print join("\t", @columns), "\n";
+}
+
+close($fh);
